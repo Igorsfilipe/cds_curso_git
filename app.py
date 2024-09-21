4661e295e3487daaaafe858ba6b9ad178e695b08
Igors Cardoso <igorsfilipe@hotmail.com>
2024-09-21 18:46:50 +0100

    Utilizado a função de descrição de dados

--- app.py
+++ app.py
@@ -38,9 +38,11 @@ def create_dataframe_section(df):
     return None
 
 def main():
-    df = load_data()
+    df_raw = load_data()
 
-    st.dataframe(df)
+    create_dataframe_section(df_raw)
+
+    st.dataframe(df_raw)
 if __name__ == '__main__':
     main()
 
